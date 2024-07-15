2c94b0fe43d42ab81d5f2960e913e92702f88670
t.karakaya <t.karakaya@tu-bs.de>
2024-07-15 20:11:34 +0200

    last function implemented

--- libft/libft.h
+++ libft/libft.h
@@ -3,6 +3,7 @@
 
 # include <stddef.h>
 # include <stdlib.h>
+#include <unistd.h>
 
 int		ft_isalnum(int);
 int		ft_isalpha(int);
@@ -33,5 +34,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
